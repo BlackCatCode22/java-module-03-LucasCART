fbcbd17efcb9743b8bf5e8c6eb32269be326f62d
Lucas Romagnoli <95435728+LucasCART@users.noreply.github.com>
2024-09-10 19:14:25 +0000

    Student java

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    public static int numOfStudents = 0;
+
+    // Create a constructor for our new objects.
+    public Student() {
+        numOfStudents++;
+    }
+
+    public static int getNumOfStudents() {
+        return numOfStudents;
+    }
+
+    // Give our Student class a few fields
+    String firstName;
+    String lastName;
+    String major;
+    double gpa;
+    int age;
+    boolean onProbation;
+
+}
