99d0e0fa57e813730447627e64043ea31ed55a5e
Lucas Romagnoli <95435728+LucasCART@users.noreply.github.com>
2024-09-12 19:23:58 +0000

    Cat java (package)

---
+++ SepCat.java
@@ -0,0 +1,12 @@
+package myanimals;
+
+public class Cat extends Animal {
+    // The Cat class is a subclass of Animal.
+    // So it should have all the methods from the Animal class available to it.
+    // This is polymorphism - this words means "many shapes" when used here, by a Cat object,
+    // this method that has the same name as the method in the superclass, takes another shape.
+    public void animalSound() {
+        System.out.println("\nA sound from the Cat class used by a Cat Object - 'meow'");
+    }
+
+}
