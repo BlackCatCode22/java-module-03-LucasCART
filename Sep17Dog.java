964de5c7f637a09892417c8bc39f14cdf912b945
Lucas Romagnoli <95435728+LucasCART@users.noreply.github.com>
2024-09-17 19:15:42 +0000

    Dog java (Sep 17)

---
+++ Sep17Dog.java
@@ -0,0 +1,52 @@
+public class Dog {
+    // Create two fields for our Dog objects.
+
+    private String name;
+    private int age;
+
+    // Create getters and setters for our fields.
+
+    // Create getter and setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Create getter and setter for age
+    public int getAge() {
+        return this.age;
+        // This would work as well because there is no ambiguity
+        // Return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Create a constructor for the Dog objects that will be created with the "new" keyword.
+    // A constructor is a special method that is called or invoked when... the Dog class is used
+    // to create an object
+    private Dog() {
+        System.out.println("\nA dog object was created!! This message is from the constructor.\n");
+    }
+
+    // Create a constructor that accepts a String data type for the Dog's name.
+    private Dog(String myName) {
+        this.name = myName;
+    }
+
+    // Create a constructor that accepts an argument when an object is created, this argument will be an int
+    private Dog(int theAge) {
+        this.age = theAge;
+    }
+
+    // Create a constructor that accepts two arguments - a String for the name, and an int for age.
+    public Dog(String someName, int someAge) {
+        // Assign name and age to our new object.
+        this.age = someAge;
+        this.name = someName;
+    }
+
+}
